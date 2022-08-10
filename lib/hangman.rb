f4546dabbf47ba089cda87d0f47ff07de3091e3e
Roger Massana <rogermassana2002@gmail.com>
2022-08-10 18:52:20 +0200

    Input char and replace it to main arr

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -8,6 +8,14 @@ def getword(contents)
 end
 word = getword(contents)
 word_arr = Array.new(word.length, '_')
-
-puts word_arr.join(' ')
 puts word
+
+while word_arr.filter{|word| word == '_'}.length > 0
+  puts "Input a character"
+  char = gets.chomp[0]
+  if word.include?(char)
+    word.each_char.with_index{|c, index| word_arr[index] = char if c == char}
+  end
+  puts word_arr.join(' ')
+end
+
