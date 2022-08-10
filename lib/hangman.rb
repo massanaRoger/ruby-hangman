97c449c607dab08b94790cf45aeec2b7877f606a
Roger Massana <rogermassana2002@gmail.com>
2022-08-10 18:43:24 +0200

    Start the game with underscores

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,7 +1,13 @@
 
 contents = File.readlines('google-10000-english-no-swears.txt')
-word = contents
-  .map {|line| line.gsub("\n", '')}
-  .filter {|line| line.length >=5 && line.length <= 12}
-  .sample
+def getword(contents)
+  contents
+    .map {|line| line.gsub("\n", '')}
+    .filter {|line| line.length >=5 && line.length <= 12}
+    .sample
+end
+word = getword(contents)
+word_arr = Array.new(word.length, '_')
+
+puts word_arr.join(' ')
 puts word
