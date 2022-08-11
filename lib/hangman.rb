c60e4db719cd77f9e1e757db352e56d0a3df09a7
Roger Massana <rogermassana2002@gmail.com>
2022-08-11 14:53:02 +0200

    Put everything to a class

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,21 +1,36 @@
+# frozen_string_literal: true
+
+# Class to play and save the game
+class Game
+  attr_reader :word, :word_arr
+
+  def initialize(contents)
+    @contents = contents
+    @word = getword
+    @word_arr = Array.new(word.length, '_')
+  end
+
+  def play_game
+    while word_arr.filter { |word| word == '_' }.length.positive?
+      puts 'Input a character'
+      char = gets.chomp[0].downcase
+      if word.include?(char)
+        word.each_char.with_index { |c, index| word_arr[index] = char if c == char }
+      end
+      puts word_arr.join(' ')
+    end
+  end
+
+  private
+
+  def getword
+    @contents
+      .map { |line| line.gsub("\n", '') }
+      .filter { |line| line.length >= 5 && line.length <= 12 }
+      .sample
+  end
+end
 
 contents = File.readlines('google-10000-english-no-swears.txt')
-def getword(contents)
-  contents
-    .map {|line| line.gsub("\n", '')}
-    .filter {|line| line.length >=5 && line.length <= 12}
-    .sample
-end
-word = getword(contents)
-word_arr = Array.new(word.length, '_')
-puts word
-
-while word_arr.filter{|word| word == '_'}.length > 0
-  puts "Input a character"
-  char = gets.chomp[0]
-  if word.include?(char)
-    word.each_char.with_index{|c, index| word_arr[index] = char if c == char}
-  end
-  puts word_arr.join(' ')
-end
-
+game = Game.new(contents)
+game.play_game
