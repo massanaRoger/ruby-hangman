b9d0a9f51b3cb9b816ed1465c22e0faf28a5e13f
Roger Massana <rogermassana2002@gmail.com>
2022-08-11 15:12:48 +0200

    Lose game if out of attempts

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -8,20 +8,39 @@ class Game
     @contents = contents
     @word = getword
     @word_arr = Array.new(word.length, '_')
+    @incorrect = 0
+    @correct = 0
+    @remaining_guesses = 11
   end
 
   def play_game
-    while word_arr.filter { |word| word == '_' }.length.positive?
+    while word_arr.filter { |word| word == '_' }.length.positive? && @remaining_guesses > 0
       puts 'Input a character'
       char = gets.chomp[0].downcase
-      if word.include?(char)
-        word.each_char.with_index { |c, index| word_arr[index] = char if c == char }
-      end
-      puts word_arr.join(' ')
+      play_round(char)
+      put_info(word_arr)
     end
+    @remaining_guesses != 0
   end
 
   private
+
+  def play_round(char)
+    if word.include?(char)
+      word.each_char.with_index { |c, index| word_arr[index] = char if c == char }
+      @correct += 1
+    else
+      @incorrect += 1
+      @remaining_guesses -= 1
+    end
+  end
+
+  def put_info(word_arr)
+    puts word_arr.join(' ')
+    puts "Incorrect attempts: #{@incorrect}"
+    puts "Correct attempts: #{@correct}"
+    puts "Remaining guesses: #{@remaining_guesses}"
+  end
 
   def getword
     @contents
@@ -33,4 +52,6 @@ end
 
 contents = File.readlines('google-10000-english-no-swears.txt')
 game = Game.new(contents)
-game.play_game
+won = game.play_game
+message = won ? 'You won!' : 'You lost!'
+puts message
